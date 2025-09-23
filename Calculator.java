ee1d96957a65fa59f5b7e5ee5abb0b98639bf35d
rsannareddy <69970429+rsannareddy@users.noreply.github.com>
2025-09-23 08:58:52 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,36 @@
+public class Calculator {
+    
+    public double add(double a, double b) {
+        return a + b;
+    }
+    
+    public double subtract(double a, double b) {
+        return a - b;
+    }
+    
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+    
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return a / b;
+    }
+    
+    public double power(double base, double exponent) {
+        return Math.pow(base, exponent);
+    }
+    
+    public double sqrt(double number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("Cannot calculate square root of negative number");
+        }
+        return Math.sqrt(number);
+    }
+    
+    public double percentage(double number, double percent) {
+        return (number * percent) / 100.0;
+    }
+}
